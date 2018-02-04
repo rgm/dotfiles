b831f9433b03b4635f26a9634a2a81b981be587b
Ryan McCuaig <ryan@ryanmccuaig.net>
2018-02-03 18:14:14 -0800

    Adds ~/.lein

---
+++ lein/profiles.clj
@@ -0,0 +1,13 @@
+{:user {
+        ; :injections [(require 'pjstadig.humane-test-output)
+        ;              (pjstadig.humane-test-output/activate!)]
+        :plugins [[com.jakemccrary/lein-test-refresh "0.16.0"]
+                  ; [pjstadig/humane-test-output "0.8.3"]
+                  [lein-ancient "0.6.14"]
+                  [lein-cljfmt "0.5.7"]]
+
+        :test-refresh {:notify-command
+                       ["terminal-notifier" "-title" "test-refresh" "-message"]
+                       ; :quiet true
+                       ; :changes-only true
+                       }}}
