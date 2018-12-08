d6b4290d2ce2e2aa30f74437a22dac827486d994
Ryan McCuaig <ryan@ryanmccuaig.net>
2018-12-08 08:54:07 -0800

    Adds yagni to global lein profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -4,7 +4,8 @@
         :plugins [[com.jakemccrary/lein-test-refresh "0.16.0"]
                   ; [pjstadig/humane-test-output "0.8.3"]
                   [lein-ancient "0.6.14"]
-                  [lein-cljfmt "0.5.7"]]
+                  [lein-cljfmt "0.5.7"]
+                  [venantius/yagni "0.1.6"]]
 
         :test-refresh {:notify-command
                        ["terminal-notifier" "-title" "test-refresh" "-message"]
