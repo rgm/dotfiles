bf2c5d959caa0f6fd6b1cd3aaab0b2c8bd6492e8
Ryan McCuaig <ryan@ryanmccuaig.net>
2019-12-26 22:52:51 -0800

    Bumps lein profile items

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,11 +1,11 @@
 {:user {
         ; :injections [(require 'pjstadig.humane-test-output)
         ;              (pjstadig.humane-test-output/activate!)]
-        :plugins [[com.jakemccrary/lein-test-refresh "0.16.0"]
+        :plugins [[com.jakemccrary/lein-test-refresh "0.24.1"]
                   ; [pjstadig/humane-test-output "0.8.3"]
                   [lein-ancient "0.6.15"]
-                  [lein-cljfmt "0.5.7"]
-                  [venantius/yagni "0.1.6"]]
+                  [lein-cljfmt "0.6.4"]
+                  [venantius/yagni "0.1.7"]]
 
         :test-refresh {:notify-command
                        ["terminal-notifier" "-title" "test-refresh" "-message"]
